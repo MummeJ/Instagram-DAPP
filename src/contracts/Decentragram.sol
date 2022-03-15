ec795f4cd28f432db56051cd4bb6cb89234b21f5
Jeremy <mummejjeremy@gmail.com>
2022-03-14 22:49:15 -0500

    First Commit

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,32 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+  string public name = "Decentragram";
+  //store Images
+  uint public imageCount = 0;
+  mapping(uint => Image) public images;
+
+  struct Image{
+    uint id;
+    string hash;
+    string description;
+    uint tip;
+    address payable author;
+  }
+  event imageCreated(
+    uint id,
+    string hash,
+    string description,
+    uint tip,
+    address payable author
+  );
+
+  //Create Images
+  function uploadImage(string memory _imgHash, string memory _description) public{
+    imageCount ++;
+    images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+
+    emit imageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+  }
+  //Tip Images
 }
